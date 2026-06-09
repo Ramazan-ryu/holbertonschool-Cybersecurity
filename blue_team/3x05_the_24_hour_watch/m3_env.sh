7c69180b2bc4d1f830bd54a8471c3e6766dfeb09
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-06-09 09:51:11 +0400

    3x05

---
+++ blue_team/3x05_the_24_hour_watch/m3_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export BASE_WORKSPACE="$(pwd)"
+
+export CATALOG_DIR="$BASE_WORKSPACE"
+export HANDOFF_DIR="$BASE_WORKSPACE"
+export BASELINE_PKG="$BASE_WORKSPACE"
+export TRIAGE_PKG="$BASE_WORKSPACE/3x03_package/triage_package"
+export ASSETS_DIR="$BASE_WORKSPACE/3x03_assets"
