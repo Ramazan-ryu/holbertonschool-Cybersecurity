3535ded1249495f308fed1cf6ae987447fc1be6d
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 02:44:22 +0400

    4. Non-Standard Admin Port

--- passive_recon/7x01_beneath_the_surface/4-admin_port.sh
+++ passive_recon/7x01_beneath_the_surface/4-admin_port.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-timeout 120 nmap -sT -T2 -p 49152-65535 admin.astralis-cloud.example | grep -w 'open' | cut -d '/' -f 1 | head -n 1
+timeout 120 nmap -sT -T2 --max-rate 100 -p 49152-65535 admin.astralis-cloud.example | grep 'open' | awk -F/ '{print $1}' | head -n 1
