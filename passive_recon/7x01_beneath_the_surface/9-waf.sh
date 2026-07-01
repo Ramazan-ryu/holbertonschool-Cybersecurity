b483aac05514a2cead0f273abf0122bf623a1c56
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 03:08:59 +0400

    9. WAF Provider

--- passive_recon/7x01_beneath_the_surface/9-waf.sh
+++ passive_recon/7x01_beneath_the_surface/9-waf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -I -m 10 portal.astralis-cloud.example | grep -i 'waf' | awk -F': ' '{print $2}' | tr -d '\r' | head -n 1
+wafw00f portal.astralis-cloud.example | grep -i 'is behind' | awk '{print $NF}' || curl -s -I -m 10 portal.astralis-cloud.example | grep -iE 'Server:|CF-RAY|X-Amz-Cf-Id|X-Served-By' | grep -vi 'nginx' | awk -F': ' '{print $2}' | tr -d '\r' | head -n 1
