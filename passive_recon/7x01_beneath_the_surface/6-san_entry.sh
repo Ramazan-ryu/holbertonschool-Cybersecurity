41573e793c9653c3f96fcf40a7086fe9e4c81bbe
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 02:56:48 +0400

    6. Internal SAN Entry

--- passive_recon/7x01_beneath_the_surface/6-san_entry.sh
+++ passive_recon/7x01_beneath_the_surface/6-san_entry.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-timeout 10 openssl s_client -connect portal.astralis-cloud.example:443 < /dev/null 2>/dev/null | openssl x509 -noout -text | grep -Eo 'DNS:[a-zA-Z0-9.-]+' | cut -d ':' -f 2 | grep -E 'internal|backend|admin|mgmt' | head -n 1
+timeout 10 openssl s_client -connect portal.astralis-cloud.example:443 -servername portal.astralis-cloud.example < /dev/null 2>/dev/null | openssl x509 -noout -text | grep -Eo 'DNS:[a-zA-Z0-9.-]+' | cut -d ':' -f 2 | grep -E 'internal|backend|admin|mgmt' | head -n 1
