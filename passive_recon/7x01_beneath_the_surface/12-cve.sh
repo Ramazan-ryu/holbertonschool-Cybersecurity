a5ddd8ee066f216d8105bdd5313aa2e58d2698e3
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 03:24:18 +0400

    12. Critical CVE

--- passive_recon/7x01_beneath_the_surface/12-cve.sh
+++ passive_recon/7x01_beneath_the_surface/12-cve.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-version=$(curl -s -I -m 10 portal.astralis-cloud.example | grep -i 'Server:' | awk '{print $2}' | tr -d '\r') && curl -s -m 10 "http://cve.astralis-cloud.example/api?keyword=$version" | jq -r '.vulnerabilities | sort_by(.cve.metrics.cvssMetricV31[0].cvssData.baseScore) | last | .cve.id'
+product="nginx"; version=$(curl -s -I -m 10 portal.astralis-cloud.example | grep -i 'Server:' | awk '{print $2}' | cut -d/ -f2 | tr -d '\r'); curl -s -m 10 "https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${product}+${version}" | jq -r '.vulnerabilities | sort_by(.cve.metrics.cvssMetricV31[0].cvssData.baseScore) | last | .cve.id' | grep 'CVE' | head -n 1
