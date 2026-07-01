ea9c1370637a4df161991fbb0d0c045d83a319d2
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 03:04:25 +0400

    8. DMARC Policy

--- passive_recon/7x01_beneath_the_surface/8-dmarc.sh
+++ passive_recon/7x01_beneath_the_surface/8-dmarc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dig +short TXT _dmarc.astralis-cloud.example | grep -Eo 'p=(none|quarantine|reject)' | cut -d '=' -f 2 | head -n 1
+dig +short TXT _dmarc.astralis-cloud.example | grep -Eo 'p=(none|quarantine|reject)' | awk -F= '{print $2}' | head -n 1
