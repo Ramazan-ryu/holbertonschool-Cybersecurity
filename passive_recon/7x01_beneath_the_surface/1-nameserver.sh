53160d9f7d466dd3d3bff3ec024bc79154a5ec22
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-02 02:16:56 +0400

    1. Primary Nameserver

--- passive_recon/7x01_beneath_the_surface/1-nameserver.sh
+++ passive_recon/7x01_beneath_the_surface/1-nameserver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-dig +short SOA astralis-cloud.example | cut -d ' ' -f 1 | grep -o '.*[^.]'
-
-dig +short SOA @10.42.82.57 | cut -d ' ' -f 1 | grep -o '.*[^.]'
+dig +short NS astralis-cloud.example | head -n 1 | sed 's/\.$//'
+dig +short NS @10.42.82.57  | head -n 1 | sed 's/\.$//'
