c604e64333033c2f1f4933d9d1c642f5d84a4e3c
Ramazan-ryu <mustafayevramazan121@gmail.com>
2026-07-23 00:59:00 +0400

    git

--- active_recon/8x00_the_cartographers_patience/5-banner.sh
+++ active_recon/8x00_the_cartographers_patience/5-banner.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl s_client -connect mail.berent.example:4650 -servername mail.berent.example -crlf -quiet
+openssl s_client -connect 10.42.173.79:4650 -servername mail.berent.example -crlf -quiet
